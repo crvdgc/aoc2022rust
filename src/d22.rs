98f108d1b9c2e890680691c71369fae4caf0c4dc
crvdgc <ubikium@gmail.com>
2023-03-27 18:01:52 +0100

    wip

--- src/d22.rs
+++ src/d22.rs
@@ -30,7 +30,7 @@ pub fn part0(input: &str, width: usize) {
     println!("{}", password);
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 enum Dir {
     Up,
     Left,
@@ -71,6 +71,15 @@ impl Dir {
             (Turn::R, Dir::Left) => Dir::Up,
             (Turn::R, Dir::Down) => Dir::Left,
             (Turn::R, Dir::Right) => Dir::Down,
+        }
+    }
+
+    fn step(&self, (r, c): (usize, usize)) -> (usize, usize) {
+        match self {
+            Dir::Up => (r - 1, c),
+            Dir::Left => (r, c - 1),
+            Dir::Down => (r + 1, c),
+            Dir::Right => (r, c + 1),
         }
     }
 
@@ -279,18 +288,92 @@ fn start_c(first_row: &[Option<bool>]) -> usize {
         .expect("No open")
 }
 
+const B: usize = 50;
+const N: usize = 3;
+const B0: usize = 0;
+const B1: usize = B;
+const B2: usize = 2 * B;
+const B3: usize = 3 * B;
+const B4: usize = 4 * B;
+
+const B1I: usize = B - 1;
+const B2I: usize = B2 - 1;
+const B3I: usize = B3 - 1;
+const B4I: usize = B4 - 1;
+
+const WIDTH: usize = B3;
+
+// a        b
 // ┌───┐  ┌─┐
-// │   1122 │
+// │ c 1122 │
 // │┌──1122┐│
-// ││  33┘ ││
-// ││ ┌33  ││
+// ││  33┘e││
+// ││f┌33 d││
 // │└5544──┘│
 // │ 5544   │
-// └─66┘    │
+// └─66┘g   │
 //   66     │
 //    └─────┘
-pub fn part1(input: &str, width: usize, block_n: usize) {}
+fn movement_part1(
+    map: &[Vec<Option<bool>>],
+    (r, c): (usize, usize),
+    dir: Dir,
+) -> Option<((usize, usize), Dir)> {
+    let in_block = |n: usize, k: usize| n >= k * B && n < (k + 1) * B;
+    let to = |r: usize, c: usize, dir: Dir| match map[r][c] {
+        Some(false) => Some(((r, c), dir)),
+        Some(true) => None,
+        None => panic!("Not valid ({}, {})", r, c),
+    };
+    match (r, c, dir) {
+        (0, c, Dir::Up) if in_block(c, 1) => to(c + B2, 0, Dir::Right), // a1
+        (0, c, Dir::Up) if in_block(c, 2) => to(B4I, c - B2, Dir::Up),  // b1
+        (r, B1, Dir::Left) if in_block(r, 0) => to(B3 - r, 0, Dir::Right), // c1
+        (r, B3I, Dir::Right) if in_block(r, 0) => to(B3 - r, B2I, Dir::Left), // d1
+        (B1I, c, Dir::Down) if in_block(c, 2) => to(c - B1, B2I, Dir::Left), // e1
+        (r, B1, Dir::Left) if in_block(r, 1) => to(B2, r - B1, Dir::Down), // f1
+        (r, B2I, Dir::Right) if in_block(r, 1) => to(B1I, r + B1, Dir::Up), // e2
+        (B2, c, Dir::Up) if in_block(c, 0) => to(c + B1, B1, Dir::Right), // f2
+        (r, 0, Dir::Left) if in_block(r, 2) => to(B3 - r, B1, Dir::Right), // c2
+        (r, B2I, Dir::Right) if in_block(r, 2) => to(B3 - r, B3I, Dir::Left), // d2
+        (B3I, c, Dir::Down) if in_block(c, 1) => to(c + B2, B1I, Dir::Left), // g1
+        (r, 0, Dir::Left) if in_block(r, 3) => to(0, r - B2, Dir::Down), // a2
+        (r, B1I, Dir::Right) if in_block(r, 3) => to(B3I, r - B2, Dir::Up), // g2
+        (B4I, c, Dir::Down) if in_block(c, 0) => to(0, c + B2, Dir::Down), // b2
+        _ => Some((dir.step((r, c)), dir)),
+    }
+}
 
+pub fn part1(input: &str) {
+    let (map, insts) = parse_input(input, WIDTH);
+    // pp_map(&map);
+    // pp_inst(&insts);
+    // let map_t = transpose(&map, width);
+    let mut p = (0, start_c(&map[0]));
+    let mut dir = Dir::Right;
+    for inst in insts {
+        match inst {
+            Inst::Move(n) => {
+                for _ in 0..n {
+                    match movement_part1(&map, p, dir) {
+                        None => {
+                            break;
+                        }
+                        Some((next_p, next_dir)) => {
+                            p = next_p;
+                            dir = next_dir;
+                        }
+                    }
+                }
+            }
+            Inst::Rotate(turn) => {
+                dir = dir.turn(turn);
+            }
+        }
+    }
+    let password = (p.0 + 1) * 1000 + (p.1 + 1) * 4 + dir.value();
+    println!("{}", password);
+}
 pub fn example_input() -> &'static str {
     r#"        ...#
         .#..
